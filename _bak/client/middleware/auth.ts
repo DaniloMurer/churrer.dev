7cc7f871944a66d3aca193336a7ac00e499abd1d
Danilo Jakob <danilojakob@protonmail.com>
2025-04-13 15:48:32 +0200

    dev(client): new client setup

--- _bak/client/middleware/auth.ts
+++
@@ -1,8 +0,0 @@
-import { useAuthStore } from '~/store/auth';
-
-export default defineNuxtRouteMiddleware((to, from) => {
-	const token = useAuthStore().token;
-	if (!token) {
-		return navigateTo('/')
-	}
-});
